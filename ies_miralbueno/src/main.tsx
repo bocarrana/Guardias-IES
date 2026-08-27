35cf470a99f22e0fa91af591f4d1c2a4266e28b8
bocarrana <alplanastorrea@gmail.com>
2026-08-28 00:14:32 +0200

    Restaurada fecha y hora real en vivo para todos los institutos

--- ies_miralbueno/src/main.tsx
+++ ies_miralbueno/src/main.tsx
@@ -6,32 +6,6 @@ import { ThemeProvider } from './context/ThemeContext';
 import App from './App';
 import './index.css';
 
-// --- MOCK DATE FOR SCHOOL YEAR SIMULATION ---
-// Shifts the global application date to Wednesday, January 14, 2026, at 09:30:00 AM (a live school day)
-const OriginalDate = globalThis.Date;
-const mockStartTime = new OriginalDate('2026-01-14T09:30:00').getTime();
-const realStartTime = OriginalDate.now();
-
-class MockDate extends OriginalDate {
-  constructor(...args: any[]) {
-    if (args.length === 0) {
-      const elapsed = OriginalDate.now() - realStartTime;
-      super(mockStartTime + elapsed);
-    } else {
-      // @ts-ignore
-      super(...args);
-    }
-  }
-}
-// @ts-ignore
-MockDate.now = () => {
-  const elapsed = OriginalDate.now() - realStartTime;
-  return mockStartTime + elapsed;
-};
-// @ts-ignore
-globalThis.Date = MockDate;
-
-
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ThemeProvider>
